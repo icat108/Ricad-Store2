724e772849fdf9dbbf8ee32cdb496045e0a3eb1b
icat108 <68917848+icat108@users.noreply.github.com>
2020-10-23 00:24:47 +0700

    Update FooterShayna.vue

--- src/components/FooterShayna.vue
+++ src/components/FooterShayna.vue
@@ -18,7 +18,6 @@
             <div class="footer-social">
               <a href="https://www.facebook.com/icat108/"><i class="fa fa-facebook"></i></a>
               <a href="https://www.instagram.com/ricadagustin108/"><i class="fa fa-instagram"></i></a>
-              <a href="#"><i class="fa fa-twitter"></i></a>
               <a href="https://id.pinterest.com/ichacnakval/_saved/"><i class="fa fa-pinterest"></i></a>
             </div>
           </div>
