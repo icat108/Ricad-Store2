a3eb8b21e94664bea9c8fad77e6bc62d9e6678cb
icat108 <68917848+icat108@users.noreply.github.com>
2020-10-23 20:48:14 +0700

    Update WomanShayna.vue

--- src/components/WomanShayna.vue
+++ src/components/WomanShayna.vue
@@ -68,7 +68,7 @@ export default {
   },
   mounted() {
     axios
-      .get("http://127.0.0.1:8000/api/products")
+      .get("http://lets-studynow.com/Ricad-Store2/api/products")
       .then(res => (this.products = res.data.data.data))
       // eslint-disable-next-line no-console
       .catch(err => console.log(err));
